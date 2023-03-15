51f77a2f1c8767a702973a57b26ec12207f0680e
Marco Hernandez <marco7245@hotmail.com>
2023-03-14 22:07:29 -0600

    Modo Oscuro Terminado

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,27 @@
 document.addEventListener('DOMContentLoaded',function(){
     eventListeners();
+    darkMode();
 });
+
+function darkMode(){
+
+    const prefieraDarkMode = window.matchMedia('(prefers-color-scheme:dark)');
+
+    prefieraDarkMode.addEventListener('change',function(){
+        if(prefieraDarkMode.matches){
+            document.body.classList.add('dark-mode');
+        }else{
+            document.body.classList.remove('dark-mode');
+        }
+    });
+
+    const botonDarkMode = document.querySelector('.dark-mode-boton');
+
+    botonDarkMode.addEventListener('click',function(){
+        document.body.classList.toggle('dark-mode');
+
+    });
+}
 
 function eventListeners(){
     const mobileMenu = document.querySelector('.mobile-menu');
