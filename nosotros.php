da801cf3e6391e650781f72f850fb6f757230ee0
Marco Hernandez <marco7245@hotmail.com>
2023-03-15 19:19:51 -0600

    Optimizando Header y Footer con php

--- nosotros.php
+++ nosotros.php
@@ -1,7 +1,8 @@
     <!-- Header -->
     <?php
-        include './includes/templates/header.php';
-    ?><!---->
+        require './includes/funciones.php';
+        incluirTemplate('header');
+    ?> <!---->
 
     <main>
         <section class="contenedor seccion">
@@ -57,20 +58,7 @@
         </section>
     </main>
 
-
-    <footer class="footer seccion">
-        <div class="contenedor contenedor-footer">
-            <nav class="navegacion">
-                <a href="nosotros.html">Nosotros</a>
-                <a href="anuncios.html">Anuncios</a>
-                <a href="blog.html">Nuestro Blog</a>
-                <a href="contacto.html">Contacto</a>
-            </nav>
-        </div>
-        <p class="copyright">Todos los derechos Reservados 2023 &copy;</p>
-    </footer>
-
-    <script src="./build/js/bundle.min.js"></script>
-</body>
-
-</html>
+    <!-- Footer -->
+    <?php
+        incluirTemplate('footer');
+    ?> <!---->
